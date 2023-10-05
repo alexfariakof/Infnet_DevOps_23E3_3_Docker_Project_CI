403033de85fce1aa55efa7c4c26ed23c8976ea95
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-05 20:25:47 -0300

    fixining implemented

---
+++ Infnet_DevOps_23E3_3_Docker_Project_Test/WeatherForecastControllerTest.cs
@@ -0,0 +1,87 @@
+﻿using Infnet_DevOps_23E3_3_Docker_Project.Controllers;
+using Infnet_DevOps_23E3_3_Docker_Project;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Infnet_DevOps_23E3_3_Docker_Project_Test
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void Get_ReturnsWeatherForecasts()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<WeatherForecast>>(result);
+        }
+
+        [Fact]
+        public void Post_ReturnsWeatherForecast()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Post();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<WeatherForecast>(result);
+        }
+
+        [Fact]
+        public void Put_ReturnsErroDiveWeatherForecast()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Put();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<WeatherForecast>(result);
+        }
+
+        [Fact]
+        public void Delete_ReturnsWeatherForecast()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Delete();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<WeatherForecast>(result);
+        }
+
+        [Fact]
+        public void TemperatureC_CalculatesCorrectly()
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecast
+            {
+                TemperatureC = 25
+            };
+
+            // Act
+            int temperatureC = weatherForecast.TemperatureC;
+
+            // Assert
+            int expectedTemperatureF = 32 + (int)(temperatureC / 0.5556);
+            Assert.Equal(expectedTemperatureF, weatherForecast.TemperatureF);
+        }
+    }
+}
--- Infnet_DevOps_23E3_3_Docker_Project_Test/WeatherForecastTest.cs
+++ Infnet_DevOps_23E3_3_Docker_Project_Test/WeatherForecastTest.cs
@@ -1,87 +1,25 @@
-﻿using Infnet_DevOps_23E3_3_Docker_Project.Controllers;
-using Infnet_DevOps_23E3_3_Docker_Project;
-using Microsoft.Extensions.Logging;
-using Moq;
+﻿using Infnet_DevOps_23E3_3_Docker_Project;
 
 namespace Infnet_DevOps_23E3_3_Docker_Project_Test
 {
-    public class WeatherForecastControllerTest
+
+    public class WeatherForecastTests
     {
-        [Fact]
-        public void Get_ReturnsWeatherForecasts()
+        [Theory]
+        [InlineData(0, 32)]
+        [InlineData(25, 76)]
+        [InlineData(100, 211)]
+        [InlineData(-10, 15)]
+        public void TemperatureF_ConvertsCorrectly(int temperatureC, int expectedTemperatureF)
         {
             // Arrange
-            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
-            var controller = new WeatherForecastController(loggerMock.Object);
+            var weatherForecast = new WeatherForecast { TemperatureC = temperatureC };
 
             // Act
-            var result = controller.Get();
+            int actualTemperatureF = weatherForecast.TemperatureF;
 
             // Assert
-            Assert.NotNull(result);
-            Assert.IsType<List<WeatherForecast>>(result);
-        }
-
-        [Fact]
-        public void Post_ReturnsWeatherForecast()
-        {
-            // Arrange
-            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
-            var controller = new WeatherForecastController(loggerMock.Object);
-
-            // Act
-            var result = controller.Post();
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<WeatherForecast>(result);
-        }
-
-        [Fact]
-        public void Put_ReturnsErroDiveWeatherForecast()
-        {
-            // Arrange
-            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
-            var controller = new WeatherForecastController(loggerMock.Object);
-
-            // Act
-            var result = controller.Put();
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<WeatherForecast>(result);
-        }
-
-        [Fact]
-        public void Delete_ReturnsWeatherForecast()
-        {
-            // Arrange
-            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
-            var controller = new WeatherForecastController(loggerMock.Object);
-
-            // Act
-            var result = controller.Delete();
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.IsType<WeatherForecast>(result);
-        }
-
-        [Fact]
-        public void TemperatureC_CalculatesCorrectly()
-        {
-            // Arrange
-            var weatherForecast = new WeatherForecast
-            {
-                TemperatureC = 25
-            };
-
-            // Act
-            int temperatureC = weatherForecast.TemperatureC;
-
-            // Assert
-            int expectedTemperatureF = 32 + (int)(temperatureC / 0.5556);
-            Assert.Equal(expectedTemperatureF, weatherForecast.TemperatureF);
+            Assert.Equal(expectedTemperatureF, actualTemperatureF);
         }
     }
 }
